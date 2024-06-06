4ba790b05d7d6799e4ed7bf0163c91453281a488
Gemiou <efthymiouge@gmail.com>
2024-06-06 10:32:40 +0300

    Remove docker and keycloak

--- app.vue
+++ app.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import Keycloak from "keycloak-js";
-import { useKeycloak } from "@/stores/keycloak";
-const config = useRuntimeConfig();
-const keycloakStore = useKeycloak();
-const state = reactive({
-  loggedIn: false,
-});
+// import Keycloak from "keycloak-js";
+// import { useKeycloak } from "@/stores/keycloak";
+// const config = useRuntimeConfig();
+// const keycloakStore = useKeycloak();
+// const state = reactive({
+//   loggedIn: false,
+// });
 
-const initOptions = {
-  url: config.public.keycloakUrl,
-  realm: config.public.keycloakRealm,
-  clientId: config.public.keycloakClientId,
-};
-const keycloak = new Keycloak(initOptions);
-keycloak
-  .init({ pkceMethod: "S256", responseMode: "query", onLoad: "check-sso" })
-  .then((auth) => {
-    if (!auth) {
-      keycloak.login();
-    } else {
-      keycloakStore.setup(keycloak);
-      state.loggedIn = true;
-    }
-  });
+// const initOptions = {
+//   url: config.public.keycloakUrl,
+//   realm: config.public.keycloakRealm,
+//   clientId: config.public.keycloakClientId,
+// };
+// const keycloak = new Keycloak(initOptions);
+// keycloak
+//   .init({ pkceMethod: "S256", responseMode: "query", onLoad: "check-sso" })
+//   .then((auth) => {
+//     if (!auth) {
+//       keycloak.login();
+//     } else {
+//       keycloakStore.setup(keycloak);
+//       state.loggedIn = true;
+//     }
+//   });
 </script>
 
 <template>
